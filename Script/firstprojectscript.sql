7cad9dbae0e4b14371815e8378b781c13cb19256
larissaemma <emamalie101@gmail.com>
2024-02-08 19:08:09 -0600

    Finished question 12

--- Script/firstprojectscript.sql
+++ Script/firstprojectscript.sql
@@ -25,9 +25,9 @@ WHERE location in ('TN','KY')
 
 SELECT count (star_rating)
 FROM data_analyst_jobs
-Where star_rating >= 4.0
+Where star_rating > 4.0
 
---answer: 573
+--answer: 416
 
 --5.How many postings in the dataset have a review count between 500 and 1000?
 
@@ -62,3 +62,42 @@ WHERE location = 'CA'
 --answer: 230
 
 --9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+
+SELECT title, avg(star_rating)
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY title 
+
+--answer: 95
+
+--10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+
+SELECT title, avg(star_rating)
+FROM data_analyst_jobs
+WHERE review_count > 5000
+GROUP BY title 
+ORDER BY avg(star_rating) desc
+
+
+--answer: "Senior Data Analyst – Marketplace Analytics – Multiple Openi..."; 4.1999998090000000
+
+
+--11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+
+SELECT Count (title)
+FROM data_analyst_jobs
+WHERE title like '%Analyst%'
+
+--answer:1636
+
+--12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title not like'%Analyst%' and title not like  '%Analytics%'
+
+SELECT count (title)
+FROM data_analyst_jobs
+WHERE title not like '%Analy%' 
+
+--answer: 39; "Analy"
